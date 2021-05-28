e1b392162722ffac11ff48ea2b0029ca95752306
Lucas Correia <tsirlucas@gmail.com>
2021-05-28 15:48:36 -0300

    better algolia objects build script

--- docusaurus/src/build-algolia-objects.js
+++ docusaurus/src/build-algolia-objects.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const mdx = require('@mdx-js/mdx');
-
+const admonitions = require('remark-admonitions');
 const {
   parseFrontMatter,
   parseMarkdownContentTitle,
@@ -25,6 +25,7 @@ const extractSyntaxTree = (path) => {
     mdx.sync(content, {
       skipExport: true,
       remarkPlugins: [
+        admonitions,
         () => (ast) => {
           res(ast);
           return ast;
@@ -32,6 +33,108 @@ const extractSyntaxTree = (path) => {
       ],
     });
   });
+};
+
+const parseMdxData = (toString, syntaxTree, lastHeaderIdParam) => {
+  let lastHeaderId = undefined;
+  const initialReduceValue = {};
+  // When calling this function recursively we will receive the
+  // "lastHeaderIdParam" param and it means we need to
+  // pre-initialize things
+  if (lastHeaderIdParam) {
+    lastHeaderId = lastHeaderIdParam;
+    initialReduceValue[lastHeaderId] = {
+      headerId: lastHeaderId,
+      text: [],
+      code: [],
+    };
+  }
+
+  // This reduce functions basically iterate through the markdown
+  // nodes and pick only paragraphs and code, categorizing them
+  // by header id. This will allow us to later have search results
+  // that leads us to specific page headers in the docs
+  return syntaxTree.children.reduce((curr, item) => {
+    // If heading, we initialize the heading object and set it
+    // as lastHeaderId so next items in the tree can use it as
+    // key
+    if (item.type === 'heading') {
+      if (
+        curr[lastHeaderId] &&
+        curr[lastHeaderId].text.length === 0 &&
+        curr[lastHeaderId].code.length === 0
+      ) {
+        // cleanup empty items from last iteration
+        delete curr[lastHeaderId];
+      }
+      lastHeaderId = toString(item).replace(/\s/g, '-');
+      curr[lastHeaderId] = { headerId: lastHeaderId, text: [], code: [] };
+    }
+
+    // If there is no heading id yet, it means that this page has some description
+    // text before starting the headers. We need to create a specific page key in order to
+    // put the text/code available in that page.
+    if (!curr[lastHeaderId]) {
+      const pageItem = parseMdxData(toString, { children: [item] }, 'page');
+
+      if (!curr.page) {
+        curr.page = {
+          text: [],
+          code: [],
+        };
+      }
+
+      curr.page.text.push(...pageItem.page.text);
+      curr.page.code.push(...pageItem.page.code);
+
+      return curr;
+    }
+
+    // Nodes with children should run recursively because if we just call
+    // "toString" method on it, it will end up returning a string containing
+    // code mdx code comments and jsx tags, etc
+    if (item.children) {
+      const admonitionItem = parseMdxData(toString, item, lastHeaderId);
+      curr[lastHeaderId].text.push(admonitionItem[lastHeaderId].text.join(' '));
+      curr[lastHeaderId].code.push(...admonitionItem[lastHeaderId].code);
+      return curr;
+    }
+
+    // If type is jsx, it can two things: It is some component being used
+    // or some html tags wrapping text. Either way we just remove
+    // any kind of jsx tag and only leave its content as text
+    // and then push it.
+    if (item.type === 'jsx') {
+      const jsxString = toString(item).replace(/(<([^>]+)>)/gi, '');
+
+      // Cleanup empty strings and solo line breaks after tags removal
+      if (jsxString !== '\n' && jsxString !== '') {
+        curr[lastHeaderId].text.push(jsxString);
+      }
+      return curr;
+    }
+
+    // If code, we just push its content
+    if (item.type === 'code') {
+      curr[lastHeaderId].code.push(toString(item));
+      return curr;
+    }
+
+    // These tags tags should be ignored
+    if (
+      item.type === 'import' &&
+      item.type === 'image' &&
+      item.type === 'comment' &&
+      item.type === 'thematicBreak'
+    ) {
+      return curr;
+    }
+
+    // Any other tag should be parsed to string
+    curr[lastHeaderId].text.push(toString(item));
+
+    return curr;
+  }, initialReduceValue);
 };
 
 const extractMdxData = async (path) => {
@@ -42,33 +145,8 @@ const extractMdxData = async (path) => {
     const { toString } = await import('mdast-util-to-string');
     const syntaxTree = await extractSyntaxTree(path);
 
-    let lastHeaderId = null;
-
-    // This reduce functions basically iterate through the markdown
-    // nodes and pick only paragraphs and code, categorizing them
-    // by header id. This will allow us to later have search results
-    // that leads us to specific page headers in the docs
-    const result = syntaxTree.children.reduce((curr, item) => {
-      if (item.type === 'heading') {
-        if (
-          curr[lastHeaderId] &&
-          curr[lastHeaderId].text.length === 0 &&
-          curr[lastHeaderId].code.length === 0
-        ) {
-          // cleanup empty items
-          delete curr[lastHeaderId];
-        }
-        lastHeaderId = toString(item).replace(' ', '-');
-        curr[lastHeaderId] = { headerId: lastHeaderId, text: [], code: [] };
-      }
-      if (!curr[lastHeaderId]) return curr;
-      if (item.type === 'paragraph')
-        curr[lastHeaderId].text.push(toString(item));
-      if (item.type === 'code') curr[lastHeaderId].code.push(toString(item));
-      return curr;
-    }, {});
-
-    return Object.values(result);
+    const { page, ...headers } = parseMdxData(toString, syntaxTree);
+    return { page, headers: Object.values(headers) };
   })();
 };
 
@@ -90,9 +168,8 @@ const extractDocsData = async (docsContent) => {
         const sectionsSlug = sections.join('/');
         // Once we dont have the header data/page content on the object generated by
         // docusaurus, we need to extract it manually from the md/mdx file
-        const pageHeaderData = await extractMdxData(
-          sdkDocVersion.source.replace('@site/', '')
-        );
+        const { page: pageData, headers: pageHeadersData } =
+          await extractMdxData(sdkDocVersion.source.replace('@site/', ''));
 
         const version = sdkDocVersion.version;
         const page_id = parseInt(`${platformIndex}${versionIndex}${pageIndex}`);
@@ -115,10 +192,11 @@ const extractDocsData = async (docsContent) => {
         const parent_section_slug = 'chat_docs';
         const parent_section_id = 'chat_docs';
         const platform = platformMapping[sdkKey];
-        const content_serialized_text = sdkDocVersion.description;
+        const content_serialized_text = pageData && pageData.text.join('\n');
+        const code_sample = pageData && pageData.code.join('\n');
         const pageObjectID = `${platformMapping[sdkKey]}-${sdkDocVersion.version}-${pageIndex}-`;
 
-        const pageData = {
+        const pageObject = {
           version,
           page_id,
           header_id: '',
@@ -132,11 +210,12 @@ const extractDocsData = async (docsContent) => {
           parent_section_id,
           platform,
           content_serialized_text,
+          code_sample,
           objectID: pageObjectID,
         };
         return [
-          pageData,
-          ...pageHeaderData.map((headerData) => {
+          pageObject,
+          ...pageHeadersData.map((headerData) => {
             const header_id = headerData.headerId
               .toLowerCase()
               .replace(/\s/g, '-');
