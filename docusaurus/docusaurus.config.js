3cd673b147a9b395f5fc5f567199863f835178c1
Tatiana Inama <tatii.oxo@gmail.com>
2021-05-31 10:25:43 +0200

    Extract admonition icons from config file

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const path = require('path');
 
 const { BASE_URL, folderMapping, languageMapping } = require('./constants');
+const Icons = require('./admonition-icons');
 
 const STREAM_SDK_DOCUSAURUS_PATH = '../docusaurus';
 
@@ -56,23 +57,23 @@ const defaultPlugins = SDK_FOLDERS.map((SDK) => {
         customTypes: {
           note: {
             ifmClass: 'note',
-            svg: '<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 13.7A6.7 6.7 0 117 .3a6.7 6.7 0 010 13.4zm0-1.4A5.3 5.3 0 107 1.7a5.3 5.3 0 100 10.6zm-.7-8.6h1.4V5H6.3V3.7zm0 2.6h1.4v4H6.3v-4z" fill="#000" fill-opacity=".4"/></svg>'
+            svg: Icons.note
           },
           info: {
             ifmClass: 'info',
-            svg: '<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><g fill="#00DFFF"><path d="M10 12.7H6c-.4 0-.7-.3-.7-.7v-.3c0-1-.4-1.9-1.1-2.6-1-1.1-1.6-2.5-1.5-4C2.8 2.3 5 .1 7.9 0H8a5.3 5.3 0 013.7 9.1c-.7.7-1 1.6-1 2.6v.3c0 .4-.3.7-.7.7zm-3.3-1.4h2.6a5 5 0 011.5-3.2c.8-.7 1.2-1.7 1.2-2.8a4 4 0 00-4-4 4 4 0 00-4 3.9 4 4 0 001.1 3c1 .9 1.5 2 1.6 3.1z"/><path d="M8 16a2.7 2.7 0 01-2.7-2.7V12c0-.4.3-.7.7-.7h4c.4 0 .7.3.7.7v1.3c0 1.5-1.2 2.7-2.7 2.7zm-1.3-3.3v.6c0 .8.6 1.4 1.3 1.4.7 0 1.3-.6 1.3-1.4v-.6H6.7zM6 6c-.4 0-.7-.3-.7-.7C5.3 4 6.5 2.7 8 2.7c.4 0 .7.2.7.6 0 .4-.3.7-.7.7-.7 0-1.3.6-1.3 1.3 0 .4-.3.7-.7.7z"/></g></svg>'
+            svg: Icons.info
           },
           tip: {
             ifmClass: 'success',
-            svg: '<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><g fill="#40df01"><path d="M10 12.7H6c-.4 0-.7-.3-.7-.7v-.3c0-1-.4-1.9-1.1-2.6-1-1.1-1.6-2.5-1.5-4C2.8 2.3 5 .1 7.9 0H8a5.3 5.3 0 013.7 9.1c-.7.7-1 1.6-1 2.6v.3c0 .4-.3.7-.7.7zm-3.3-1.4h2.6a5 5 0 011.5-3.2c.8-.7 1.2-1.7 1.2-2.8a4 4 0 00-4-4 4 4 0 00-4 3.9 4 4 0 001.1 3c1 .9 1.5 2 1.6 3.1z"/><path d="M8 16a2.7 2.7 0 01-2.7-2.7V12c0-.4.3-.7.7-.7h4c.4 0 .7.3.7.7v1.3c0 1.5-1.2 2.7-2.7 2.7zm-1.3-3.3v.6c0 .8.6 1.4 1.3 1.4.7 0 1.3-.6 1.3-1.4v-.6H6.7zM6 6c-.4 0-.7-.3-.7-.7C5.3 4 6.5 2.7 8 2.7c.4 0 .7.2.7.6 0 .4-.3.7-.7.7-.7 0-1.3.6-1.3 1.3 0 .4-.3.7-.7.7z"/></g></svg>'
+            svg: Icons.tip
           },
           caution: {
             ifmClass: 'warning',
-            svg: '<svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg"><g fill="#FECF29"><path d="M16.3 15.3H1.6A.7.7 0 011 15a.6.6 0 010-.7L8.4 1c.2-.4.9-.4 1.2 0l7.3 13.3v.7c-.1.2-.3.3-.6.3zM2.8 14h12.4L9 2.7 2.8 14z"/><path d="M9 10.7c-.4 0-.7-.3-.7-.7V6.7c0-.4.3-.7.7-.7.4 0 .6.3.6.7V10c0 .4-.2.7-.6.7zM9.4 11.5a.7.7 0 11-1 1 .7.7 0 011-1z"/><path d="M9 13a1 1 0 01-1-1c0-.5.4-1 1-1 .5 0 1 .5 1 1s-.5 1-1 1zm0-1.3c-.2 0-.4.1-.4.3 0 .2.2.3.4.3s.3-.1.3-.3c0-.2-.1-.3-.3-.3z"/></g></svg>'
+            svg: Icons.caution
           },
           danger: {
             ifmClass: 'danger',
-            svg: '<svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 15.3a5 5 0 005-5 5 5 0 00-.4-1.6c-1 1-2 1.6-2.5 1.6 2.7-4.6 1.2-6.6-2.8-9.3.3 3.3-1.9 4.8-2.8 5.7A5 5 0 009 15.3zm.4-11.8c2.2 1.8 2.2 3.2.5 6.2-.5.9.2 2 1.2 2 .5 0 1-.2 1.4-.4a3.7 3.7 0 11-6-3.6l.5-.5.7-.7a7 7 0 001.7-3z" fill="#FE2929"/></svg>'
+            svg: Icons.danger
           }
         }
 
