8bb90c6fd16d50ee3082a2c666d9563811d04c32
Lucas Correia <tsirlucas@gmail.com>
2021-05-31 10:12:09 -0300

    make algolia env vars optional

--- docusaurus/src/define-env-vars-plugin.js
+++ docusaurus/src/define-env-vars-plugin.js
@@ -1,15 +1,21 @@
 const webpack = require('webpack');
-const environemnt = require('./environment');
+const environment = require('./environment');
 
 // This plugin defines which env vars will be exposed to the webpack build
 // We are limiting ourselves to the ones declared in the environment.js file
+
+const OPTIONAL_VARIABLES = ['ALGOLIA_APP_ID', 'ALGOLIA_API_KEY'];
+
+const filteredVariables = Object.keys(environment).filter((item) =>
+  OPTIONAL_VARIABLES.includes(item) ? !!process.env[item] : true
+);
 
 module.exports = function () {
   return {
     name: 'define-env-vars-plugin',
     configureWebpack() {
       return {
-        plugins: [new webpack.EnvironmentPlugin(Object.keys(environemnt))],
+        plugins: [new webpack.EnvironmentPlugin(filteredVariables)],
         module: {
           rules: [],
         },
