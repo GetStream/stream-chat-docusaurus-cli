acb27fa10247c0445701b8010926043545d906fd
lucas correia <tsirlucas@gmail.com>
2021-06-30 19:21:15 -0300

    fix window access error

--- docusaurus/src/theme/Layout.js
+++ docusaurus/src/theme/Layout.js
@@ -4,10 +4,12 @@ import OriginalLayout from '@theme-original/Layout';
 import { ToastContainer } from 'react-toastify';
 import { AuthContextProvider } from '../contexts/AuthContext';
 
+const isBrowser = typeof window !== `undefined`;
 const isProdBuild = process.env.NODE_ENV === 'production';
 
 export default function Layout(props) {
-  const isRootPath = window.location.pathname === '/chat/docs/sdk/';
+  const isRootPath =
+    isBrowser && window.location.pathname === '/chat/docs/sdk/';
   const [canRender, setCanRender] = useState(!isProdBuild || !isRootPath);
   // whick redirect for home page. this should happen here in order to avoid
   // rendering the layout when redirecting.
