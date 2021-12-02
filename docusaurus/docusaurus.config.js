b0106d3c6b25e14080014b6f04f97a827033a879
Mads Røskar <mads.roskar@getstream.io>
2021-12-02 16:36:07 +0100

    Revert "Format the docusaurus config with prettier (default config)"
    This reverts commit 9f583bf1aac965416f6412214527426dcb609021.

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -1,18 +1,18 @@
 /* eslint-disable no-undef */
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 
-const STREAM_SDK_DOCUSAURUS_PATH = "../docusaurus";
+const STREAM_SDK_DOCUSAURUS_PATH = '../docusaurus';
 
-require("dotenv").config({
+require('dotenv').config({
   path: __dirname + `/${STREAM_SDK_DOCUSAURUS_PATH}/.env`,
 });
 
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const { folderMapping, IGNORED_DIRECTORIES } = require("./constants");
-const URLS = require("./urls");
-const Icons = require("./admonition-icons");
+const { folderMapping, IGNORED_DIRECTORIES } = require('./constants');
+const URLS = require('./urls');
+const Icons = require('./admonition-icons');
 
 const CUSTOM_PLUGIN_REGEX = /^docusaurus.*\.plugin.js$/;
 
@@ -34,23 +34,21 @@ const CUSTOM_PLUGINS = CUSTOM_PLUGIN_FILES.map((file) => {
   return sdkConfig.plugins;
 }).flat();
 
-const CUSTOM_CSS_PATH = path.join(__dirname, "src/css/components");
+const CUSTOM_CSS_PATH = path.join(__dirname, 'src/css/components');
 const CUSTOM_CSS_FILES = fs
   .readdirSync(CUSTOM_CSS_PATH)
   .map((file) => `${CUSTOM_CSS_PATH}/${file}`);
 
-const findCustomPluginIndex = (pluginId) =>
-  CUSTOM_PLUGINS.findIndex((plugin) => plugin[0] === pluginId);
-const extractCustomPlugin = (pluginId) =>
-  CUSTOM_PLUGINS.splice(findCustomPluginIndex(pluginId), 1).pop();
+const findCustomPluginIndex = (pluginId) => CUSTOM_PLUGINS.findIndex(plugin => plugin[0] === pluginId)
+const extractCustomPlugin = (pluginId) => CUSTOM_PLUGINS.splice(findCustomPluginIndex(pluginId), 1).pop();
 
 const defaultPlugins = SDK_FOLDERS.map((SDK) => {
-  const strippedSDK = SDK.toLowerCase().replace(" ", "");
+  const strippedSDK = SDK.toLowerCase().replace(' ', '');
   const sidebarPathWithoutExtension = `${STREAM_SDK_DOCUSAURUS_PATH}/sidebars-${folderMapping[
     strippedSDK
   ]
     .toLowerCase()
-    .replace(" ", "-")}`;
+    .replace(' ', '-')}`;
 
   const jsSidebarPath = `${sidebarPathWithoutExtension}.js`;
   const jsonSidebarPath = `${sidebarPathWithoutExtension}.json`;
@@ -59,52 +57,53 @@ const defaultPlugins = SDK_FOLDERS.map((SDK) => {
     ? jsSidebarPath
     : jsonSidebarPath;
 
-  const pluginId = "@docusaurus/plugin-content-docs";
+  const pluginId = '@docusaurus/plugin-content-docs';
 
   const defaultConfiguration = {
-    sidebarItemsGenerator: async function ({
-      defaultSidebarItemsGenerator,
-      ...args
-    }) {
-      const sidebarItems = await defaultSidebarItemsGenerator(args);
-      return sidebarItems.filter((item) => {
-        return !IGNORED_DIRECTORIES.includes(item.label);
-      });
-    },
-    id: strippedSDK,
-    path: `${STREAM_SDK_DOCUSAURUS_PATH}/docs/${SDK}`,
-    routeBasePath: strippedSDK,
-    ...(fs.existsSync(sidebarPath)
-      ? {
-          sidebarPath: require.resolve(sidebarPath),
-        }
-      : {}),
-    admonitions: {
-      infima: true,
-      customTypes: {
-        note: {
-          ifmClass: "note",
-          svg: Icons.note,
-        },
-        tip: {
-          ifmClass: "tip",
-          svg: Icons.tip,
-        },
-        info: {
-          ifmClass: "info",
-          svg: Icons.info,
-        },
-        caution: {
-          ifmClass: "warning",
-          svg: Icons.caution,
-        },
-        danger: {
-          ifmClass: "danger",
-          svg: Icons.danger,
+      sidebarItemsGenerator: async function ({
+        defaultSidebarItemsGenerator,
+        ...args
+      }) {
+        const sidebarItems = await defaultSidebarItemsGenerator(args);
+        return sidebarItems.filter((item) => {
+          return !IGNORED_DIRECTORIES.includes(item.label);
+        });
+      },
+      id: strippedSDK,
+      path: `${STREAM_SDK_DOCUSAURUS_PATH}/docs/${SDK}`,
+      routeBasePath: strippedSDK,
+      ...(fs.existsSync(sidebarPath)
+        ? {
+            sidebarPath: require.resolve(sidebarPath),
+          }
+        : {}),
+      admonitions: {
+        infima: true,
+        customTypes: {
+          note: {
+            ifmClass: 'note',
+            svg: Icons.note,
+          },
+          tip: {
+            ifmClass: 'tip',
+            svg: Icons.tip,
+          },
+          info: {
+            ifmClass: 'info',
+            svg: Icons.info,
+          },
+          caution: {
+            ifmClass: 'warning',
+            svg: Icons.caution,
+          },
+          danger: {
+            ifmClass: 'danger',
+            svg: Icons.danger,
+          },
         },
       },
-    },
-  };
+    }
+
 
   /**
    * Merge configuration from a custom plugin file if one exists
@@ -115,42 +114,45 @@ const defaultPlugins = SDK_FOLDERS.map((SDK) => {
    * */
   const customConfiguration = extractCustomPlugin(pluginId).pop();
 
-  return [pluginId, { ...defaultConfiguration, ...customConfiguration }];
+  return [
+    pluginId,
+    {...defaultConfiguration, ...customConfiguration},
+  ];
 });
 
 const navbarSDKItems = SDK_FOLDERS.map((SDK) => {
-  const strippedSDK = SDK.toLowerCase().replace(" ", "");
+  const strippedSDK = SDK.toLowerCase().replace(' ', '');
   const readableSDK = folderMapping[strippedSDK] || SDK;
   return {
     label: readableSDK,
     id: strippedSDK,
     to: `${strippedSDK}/`,
-    type: "doc",
+    type: 'doc',
   };
 });
 
 const navbarVersionItems = SDK_FOLDERS.map((SDK) => ({
-  docsPluginId: SDK.toLowerCase().replace(" ", ""),
-  type: "docsVersionDropdown",
-  className: "navbar__link__custom-dropdown--version",
+  docsPluginId: SDK.toLowerCase().replace(' ', ''),
+  type: 'docsVersionDropdown',
+  className: 'navbar__link__custom-dropdown--version',
 }));
 
 const navbarGithubItems = navbarSDKItems.map(({ id }) => ({
   href: URLS.github[id],
   platform: id,
-  label: "github",
-  position: "left",
-  className: "navbar__link__github",
-  "aria-label": "Github repository",
+  label: 'github',
+  position: 'left',
+  className: 'navbar__link__github',
+  'aria-label': 'Github repository',
   mobile: false,
 }));
 
 const navbarItems = [
   {
     href: URLS.website.signup,
-    label: "Sign Up",
-    position: "right",
-    className: "navbar__link__sign-up",
+    label: 'Sign Up',
+    position: 'right',
+    className: 'navbar__link__sign-up',
     mobile: false,
   },
 ];
@@ -158,78 +160,81 @@ const navbarItems = [
 if (navbarSDKItems.length > 1) {
   navbarItems.push({
     items: navbarSDKItems,
-    label: "SDK",
-    className: "navbar__link__custom-dropdown--sdks",
-    position: "left",
+    label: 'SDK',
+    className: 'navbar__link__custom-dropdown--sdks',
+    position: 'left',
   });
 }
 
-navbarItems.push(...navbarVersionItems, ...navbarGithubItems);
+navbarItems.push(
+  ...navbarVersionItems,
+  ...navbarGithubItems
+);
 
 const plugins = [...defaultPlugins, ...CUSTOM_PLUGINS];
 
-if (process.env.DEPLOYMENT_ENV === "production") {
-  plugins.push("@docusaurus/plugin-google-gtag");
+if (process.env.DEPLOYMENT_ENV === 'production') {
+  plugins.push('@docusaurus/plugin-google-gtag');
 }
 
 plugins.push(
-  "@docusaurus/plugin-content-pages",
-  "docusaurus-plugin-sass",
-  path.resolve(__dirname, "src/symlink-docusaurus"),
-  path.resolve(__dirname, "src/define-env-vars-plugin"),
-  path.resolve(__dirname, "src/build-algolia-objects")
+  '@docusaurus/plugin-content-pages',
+  'docusaurus-plugin-sass',
+  path.resolve(__dirname, 'src/symlink-docusaurus'),
+  path.resolve(__dirname, 'src/define-env-vars-plugin'),
+  path.resolve(__dirname, 'src/build-algolia-objects')
 );
 
 module.exports = {
   baseUrl: URLS.docs.root,
   trailingSlash: true,
-  favicon: "https://getstream.imgix.net/images/favicons/favicon-96x96.png",
-  onBrokenLinks: "warn",
-  onBrokenMarkdownLinks: "warn",
-  organizationName: "GetStream",
+  favicon: 'https://getstream.imgix.net/images/favicons/favicon-96x96.png',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  organizationName: 'GetStream',
   plugins,
-  projectName: "stream-chat",
-  tagline: "Stream Chat official component SDKs",
+  projectName: 'stream-chat',
+  tagline: 'Stream Chat official component SDKs',
   themeConfig: {
     // Docusaurus forces us to pass these values even if they are not internally used.
     // Theyre only used to show/hide the search bar in our case.
     algolia: {
-      appId: "MOCK",
-      apiKey: "MOCK",
-      indexName: "MOCK",
+      appId: 'MOCK',
+      apiKey: 'MOCK',
+      indexName: 'MOCK',
     },
     colorMode: {
       disableSwitch: true,
     },
     gtag: {
-      trackingID: process.env.GOOGLE_TAG_TRACKING_ID || "DEFAULT",
+      trackingID: process.env.GOOGLE_TAG_TRACKING_ID || 'DEFAULT',
     },
     liveCodeBlock: {
-      playgroundPosition: "bottom",
+      playgroundPosition: 'bottom',
     },
     navbar: {
       items: navbarItems,
       logo: {
-        alt: "Chat docs logo",
-        src: "img/logo.svg",
+        alt: 'Chat docs logo',
+        src: 'img/logo.svg',
       },
-      title: "Chat Messaging",
+      title: 'Chat Messaging',
     },
-    metadatas: [{ name: "twitter:card", content: "summary_large_image" }],
+    metadatas: [{ name: 'twitter:card', content: 'summary_large_image' }],
   },
   themes: [
     [
-      "@docusaurus/theme-classic",
+      '@docusaurus/theme-classic',
       {
         customCss: [
-          require.resolve("./src/css/custom.scss"),
+          require.resolve('./src/css/custom.scss'),
           ...CUSTOM_CSS_FILES,
         ],
       },
     ],
-    "@docusaurus/theme-live-codeblock",
-    "@docusaurus/theme-search-algolia",
+    '@docusaurus/theme-live-codeblock',
+    '@docusaurus/theme-search-algolia',
   ],
-  title: "Stream Chat - Component SDK Docs",
+  title: 'Stream Chat - Component SDK Docs',
   url: URLS.website.root,
 };
