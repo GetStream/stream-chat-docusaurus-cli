6543a7bba36574f6a80a6e2cc28880283e9a2d70
Petyo Ivanov <underlog@gmail.com>
2022-12-06 09:55:13 +0200

    chore: go syntax highlight

--- src/theme/prism-include-languages.js
+++ src/theme/prism-include-languages.js
@@ -4,8 +4,8 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
-import siteConfig from "@generated/docusaurus.config";
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import siteConfig from '@generated/docusaurus.config';
 
 const prismIncludeLanguages = (PrismObject) => {
   if (ExecutionEnvironment.canUseDOM) {
@@ -16,13 +16,13 @@ const prismIncludeLanguages = (PrismObject) => {
     additionalLanguages.forEach((lang) => {
       require(`prismjs/components/prism-${lang}`); // eslint-disable-line
     });
-    require("prismjs/components/prism-kotlin");
-    require("prismjs/components/prism-swift");
-    require("prismjs/components/prism-java");
-    require("prismjs/components/prism-dart");
-    require("prismjs/components/prism-groovy");
-    require("prismjs/components/prism-ruby");
-
+    require('prismjs/components/prism-kotlin');
+    require('prismjs/components/prism-swift');
+    require('prismjs/components/prism-java');
+    require('prismjs/components/prism-dart');
+    require('prismjs/components/prism-groovy');
+    require('prismjs/components/prism-ruby');
+    require('prismjs/components/prism-go');
     delete window.Prism;
   }
 };
