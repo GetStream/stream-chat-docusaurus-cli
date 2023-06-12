e09e5d404ac56c9b477b650e39abe6a30887b57a
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2023-06-12 15:16:00 +0200

    Disable Video ReactNative, not Chat.

--- src/product-variables.js
+++ src/product-variables.js
@@ -38,7 +38,7 @@ module.exports = {
       flutter: `${GITHUB_ROOT}/stream-chat-flutter/`,
       ios: `${GITHUB_ROOT}/stream-chat-swift/`,
       react: `${GITHUB_ROOT}/stream-chat-react/`,
-      // reactnative: `${GITHUB_ROOT}/stream-chat-react-native/`,
+      reactnative: `${GITHUB_ROOT}/stream-chat-react-native/`,
       angular: `${GITHUB_ROOT}/stream-chat-angular/`,
     },
     docusaurus: {
@@ -59,7 +59,7 @@ module.exports = {
       flutter: `${GITHUB_ROOT}/stream-video-flutter/`,
       ios: `${GITHUB_ROOT}/stream-video-swift/`,
       react: `${GITHUB_ROOT}/stream-video-js/`,
-      reactnative: `${GITHUB_ROOT}/stream-video-js/`,
+      // reactnative: `${GITHUB_ROOT}/stream-video-js/`,
       angular: `${GITHUB_ROOT}/stream-video-js/`,
     },
     docusaurus: {
