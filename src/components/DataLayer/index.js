ef6446b9eebb590309b6c3fd383fd2c55496d33e
Jaap <jaap@getstream.io>
2023-08-08 10:42:43 +0200

    add platform to dataLayer

--- src/components/DataLayer/index.js
+++ src/components/DataLayer/index.js
@@ -1,30 +1,39 @@
-import React, { useContext, useEffect } from 'react';
-import { useLocation } from '@docusaurus/router';
-import { AuthContext } from '../../contexts/AuthContext';
+import React, { useContext, useEffect } from "react"
 
-export const getPlanName = (name) => {
-  let isEnterprise = false;
-  if (!name) return ['', isEnterprise];
-  if (name.toLowerCase().includes('enterprise')) {
-    isEnterprise = true;
+import { useLocation } from "@docusaurus/router"
+
+import { AuthContext } from "../../contexts/AuthContext"
+
+export const getPlanName = name => {
+  let isEnterprise = false
+  if (!name) return ["", isEnterprise]
+  if (name.toLowerCase().includes("enterprise")) {
+    isEnterprise = true
   }
-  return [name.replace(/(v)([1-9])(_)/, ''), isEnterprise];
-};
+  return [name.replace(/(v)([1-9])(_)/, ""), isEnterprise]
+}
 
 export const DataLayer = ({}) => {
   const dataLayer =
-    typeof window !== 'undefined' ? window?.dataLayer : undefined;
-  const location = useLocation();
+    typeof window !== "undefined" ? window?.dataLayer : undefined
+  const location = useLocation()
   const { isLoggedIn, username, getSelectedOrg, email } =
-    useContext(AuthContext);
-  const org = getSelectedOrg();
+    useContext(AuthContext)
+  const org = getSelectedOrg()
+
+  const platform = useMemo(() => {
+    const [urlPlatform] = location.pathname
+      .replace(URLS.docs.root, "")
+      .split("/")
+    return urlPlatform
+  }, [location.pathname])
 
   useEffect(() => {
-    const [chatPlan, isChatEnterprise] = getPlanName(org?.chat_plan?.name);
-    const [feedsPlan, isFeedsEnterprise] = getPlanName(org?.plan?.name);
+    const [chatPlan, isChatEnterprise] = getPlanName(org?.chat_plan?.name)
+    const [feedsPlan, isFeedsEnterprise] = getPlanName(org?.plan?.name)
     const dataLayerObject = {
-      event: 'pageview',
-      authenticated: isLoggedIn ? 'authenticated' : 'anonymous',
+      event: "pageview",
+      authenticated: isLoggedIn ? "authenticated" : "anonymous",
       username,
       email,
 
@@ -32,9 +41,10 @@ export const DataLayer = ({}) => {
       feedsPlan,
       isFeedsEnterprise,
       chatPlan,
+      platform,
       isChatEnterprise,
-    };
-    dataLayer?.push(dataLayerObject);
-  }, [dataLayer, isLoggedIn, location]);
-  return null;
-};
+    }
+    dataLayer?.push(dataLayerObject)
+  }, [dataLayer, isLoggedIn, location])
+  return null
+}
