7edea3b6123c2cc9c1f5b5e035bc7bc3b3423973
Vishal Narkhede <vishalnarkhede.iitd@gmail.com>
2023-08-28 22:26:26 +0200

    feat: diff highlight for codeblocks

--- src/theme/prism-include-languages.js
+++ src/theme/prism-include-languages.js
@@ -10,12 +10,25 @@ import siteConfig from "@generated/docusaurus.config"
 const prismIncludeLanguages = PrismObject => {
   if (ExecutionEnvironment.canUseDOM) {
     const {
-      themeConfig: { prism: { additionalLanguages = [] } = {} },
+      themeConfig: {
+        prism: { additionalLanguages = [], magicComments = [] } = {},
+      },
     } = siteConfig
     window.Prism = PrismObject
     additionalLanguages.forEach(lang => {
       require(`prismjs/components/prism-${lang}`) // eslint-disable-line
     })
+    magicComments.push({
+      className: "docusaurus-highlight-removed-code-lines",
+      line: "Next line is removed",
+      block: { start: "Removed lines start", end: "Removed lines end" },
+    })
+    magicComments.push({
+      className: "docusaurus-highlight-added-code-lines",
+      line: "Next line is added",
+      block: { start: "Added lines start", end: "Added lines end" },
+    })
+
     require("prismjs/components/prism-kotlin")
     require("prismjs/components/prism-swift")
     require("prismjs/components/prism-java")
