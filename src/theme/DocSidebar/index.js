8c42d194adecdaa396fec712ea9521a2ae4781da
Drew Sartorius <duckydesign@gmail.com>
2023-08-31 16:51:34 -0500

    Add link to sidebar

--- src/theme/DocSidebar/index.js
+++ src/theme/DocSidebar/index.js
@@ -1,13 +1,30 @@
 import React, { useEffect, useMemo } from "react"
 
+import { useLocation } from "@docusaurus/router"
 import OriginalDocSidebar from "@theme-original/DocSidebar"
 
 import { useBreadcrumbsContext } from "../../hooks/useBreadcrumbsContext"
+import productVariables from "../../product-variables"
+
+const gitLink = () => {
+  const location = useLocation()
+  const locationPlatform = useMemo(() => {
+    const [urlPlatform] = location.pathname
+      .replace(URLS.docs.root, "")
+      .split("/")
+    return urlPlatform
+  }, [location.pathname])
+  return (
+    productVariables[process.env.PRODUCT]?.gitHub?.[locationPlatform] ||
+    "https://github.com/GetStream/"
+  )
+}
 
 const WEB_LINKS = [
   ["Contact Support", "https://getstream.io/contact/support/"],
   ["Maker Account", "https://getstream.io/maker-account/"],
   ["Mobile Chat Kit", "https://getstream.io/chat/ux-kit/"], // activity feeds ux kit ???
+  ["Install from GitHub", gitLink()],
   [
     <>
       © Stream.IO, Inc. <br /> All Rights Reserved.
